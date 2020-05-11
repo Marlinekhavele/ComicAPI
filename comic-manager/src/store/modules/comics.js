de6f7eb8e436c710b2bee0d4c8b7884f7aad88fc
MarlineKhavele <marlinekhavele6@gmail.com>
2020-05-11 09:13:17 +0300

    Added env

--- comic-manager/src/store/modules/comics.js
+++ comic-manager/src/store/modules/comics.js
@@ -15,30 +15,6 @@ export default {
   actions: {
     fetchComics({ commit }) {
       commit("SET_LOADING_STATUS", true, { root: true });
-      axios.get("http://52.49.227.229/api/comic/?format=json", {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        proxy: {
-          host: '52.49.227.229',
-          port: 8080
-        }
-      }).then((res) => {
-        commit("SET_LOADING_STATUS", false, { root: true });
-        commit("SET_COMICS", res.data);
-        console.log('response is : ' + res.data);
-      }).catch(function (error) {
-        if (error.response) {
-          console.log(error.response.headers);
-        }
-        else if (error.request) {
-          console.log(error.request);
-        }
-        else {
-          console.log(error.message);
-        }
-        console.log(error.config);
-      });
       axios
         .get("http://52.49.227.229/api/comic/?format=json")
         .then((res) => {
