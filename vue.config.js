9c107d5ac2476b4b0e91ad1a459930efda09943d
MarlineKhavele <marlinekhavele6@gmail.com>
2020-05-11 10:16:58 +0300

    Added config file

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    publicPath: '/ComicAPI/',
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8080,
+        https: true,
+        hotOnly: false,
+    },
+}
